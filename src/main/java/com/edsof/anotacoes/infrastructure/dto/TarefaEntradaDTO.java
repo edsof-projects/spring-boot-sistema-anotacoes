bcec5a44b481f1b77aeed999619d7fd353bbab43
Edi Aquino de Souza <edsouzza@hotmail.com>
2026-01-27 14:38:59 -0300

    atualizacao no backend das tarefas

--- src/main/java/com/edsof/anotacoes/infrastructure/dto/TarefaEntradaDTO.java
+++ src/main/java/com/edsof/anotacoes/infrastructure/dto/TarefaEntradaDTO.java
@@ -1,7 +1,11 @@
 package com.edsof.anotacoes.infrastructure.dto;
 
+import java.time.LocalDate;
+
 public record TarefaEntradaDTO(
         String titulo,
         String historico,
+        LocalDate data_fechamento,
         Long usuarioId
+
 ) {}
